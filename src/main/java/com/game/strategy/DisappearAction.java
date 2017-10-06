9b93bd890061810adeeed10b83b7ffcef9e06626
Sudhanshu Siddh <ssiddh@iu.edu>
2017-10-06 19:08:24 -0400

    F2017A6T1-16 - http://tintin.cs.indiana.edu:8091/browse/F2017A6T1-16 - Created concrete disappear action

---
+++ src/main/java/com/game/strategy/DisappearAction.java
@@ -0,0 +1,44 @@
+package com.game.strategy;
+
+import java.awt.Rectangle;
+import java.io.Serializable;
+
+import com.game.model.GameModel;
+import com.game.model.Sprite;
+
+public class DisappearAction implements ActionInterface, Serializable {
+	
+	private static final long serialVersionUID = 4L;
+	
+	private GameModel gameModel;
+	
+	public DisappearAction(GameModel gameModel){
+		this.gameModel = gameModel;
+	}
+
+	@Override
+	public void performAction(Sprite gameSprite) {
+		
+		if(gameSprite.isCollidable() && gameSprite.isVisible()){
+			
+			Rectangle currentSpriteCollider = gameSprite.createCollider();
+			
+			for (Sprite listSprite : gameModel.getSpriteList()){
+				
+				Rectangle listSpriteCollider = listSprite.createCollider();
+				
+				if(listSpriteCollider.intersects(currentSpriteCollider) && !listSprite.equals(gameSprite)){
+					//TODO Logic for changing Velocity?
+					
+					gameSprite.setVisible(false);
+					
+					//Logic for Sound effect
+				}
+				
+			}
+			
+		}
+
+	}
+
+}
