c7c7d99219a69a7aa3ecd14c4aed9a6b8fdf312f
Sudhanshu Siddh <ssiddh@iu.edu>
2017-10-07 14:19:48 -0400

    F2017A6T1-19 - http://tintin.cs.indiana.edu:8091/browse/F2017A6T1-19 - Added skeleton code for GamePlayPanel to draw sprites

--- src/main/java/com/game/view/GamePlayPanel.java
+++ src/main/java/com/game/view/GamePlayPanel.java
@@ -1,7 +1,40 @@
 package com.game.view;
 
+import java.awt.Graphics;
+
 import javax.swing.JPanel;
 
+import org.apache.log4j.Logger;
+
+import com.game.model.GameModel;
+import com.game.model.Sprite;
+
 public class GamePlayPanel extends JPanel {
+	
+	private GameModel gameModel;
+	
+	private static Logger gamePlayPanelLog = Logger.getLogger("gamePlayPanelLogger");
+	
+	
+	public GamePlayPanel(GameModel gameModel){
+		this.gameModel = gameModel;
+		this.setFocusable(true);
+		this.requestFocusInWindow();
+		this.setFocusTraversalKeysEnabled(false);
+	}
+	
+	public void paintComponent(Graphics g){
+		super.paintComponent(g);
+		
+		try{
+			for(Sprite sprite : gameModel.getSpriteList()){
+				if(sprite.isVisible()){
+					sprite.draw(g);
+				}
+			}
+		}catch(NullPointerException e){
+			gamePlayPanelLog.error("Failed to get array " + e.getLocalizedMessage());
+		}
+	}
 
 }
