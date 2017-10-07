9ac5154300254e6309310acbadbdf4622e13dca4
Seohyun Park <sp60@iu.edu>
2017-10-07 13:45:23 -0400

    F2017A6T1-18 - http://tintin.cs.indiana.edu:8091/projects/F2017A6T1/issues/F2017A6T1-18
    JUnit Test cases.

---
+++ src/test/java/com/game/model/SpriteTest.java
@@ -0,0 +1,42 @@
+package com.game.model;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class SpriteTest {
+	
+	private Sprite sprite = new Sprite();
+	
+	@Test
+	public void testTheGetterAndSetter() {
+		assertEquals("They are not equal", sprite.getHeight(), 0);
+		
+		assertEquals("They are not equal", sprite.getWidth(), 0);
+		
+		assertEquals("They are not equal", sprite.getPosition_X(), 300);
+		
+		assertEquals("They are not equal", sprite.getPosition_Y(), 300);
+	
+		assertEquals("They are not equal", sprite.getVel_X(), 10);
+		
+		assertEquals("They are not equal", sprite.getVel_Y(), 10);
+		
+		assertEquals("They are not equal", sprite.getActionInterface(), null);
+	
+		assertEquals("They are not equal", sprite.isHorizontal(), false);
+		
+		assertEquals("They are not equal", sprite.isVertical(), false);
+		
+		assertEquals("They are not equal", sprite.isRandom(), false);
+		
+		assertEquals("They are not equal", sprite.isCollidable(), false);
+		
+		assertEquals("They are not equal", sprite.isVisible(), true);
+		
+		assertEquals("They are not equal", sprite.getKeyCode(), 1000);
+		
+		assertEquals("They are not equal", sprite.isShooter(), false);
+	}
+
+}
