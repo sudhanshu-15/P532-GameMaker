e941864189e145ded69adcce714ff87e3b7efcb0
Sudhanshu Siddh <ssiddh@iu.edu>
2017-10-07 17:17:21 -0400

    F2017A6T1-17 - http://tintin.cs.indiana.edu:8091/browse/F2017A6T1-17 - Added skeleton code for Background button listener and background sound button listener

---
+++ src/main/java/com/game/helpers/SoundButtonListener.java
@@ -0,0 +1,17 @@
+package com.game.helpers;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SoundButtonListener implements ActionListener {
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		String commandString = e.getActionCommand();
+		switch(commandString){
+
+		}
+	}
+
+}
