9c59f9986e08ec520f41c880a8738a588b440339
Niteesh01 <niteesh.iit@gmail.com>
2017-10-06 21:28:47 -0400

    F2017A6T1-9:http://tintin.cs.indiana.edu:8091/projects/F2017A6T1/issues/F2017A6T1-9?filter=allopenissues
    "Adding More Constants"

--- src/main/java/com/game/helpers/Constants.java
+++ src/main/java/com/game/helpers/Constants.java
@@ -8,6 +8,101 @@ package com.game.helpers;
  *
  */
 public class Constants {
+
+
+		
+		//size of frame
+		public static int GAMEFRAME_WIDTH=1400;
+		public static int GAMEFRAME_HEIGHT=700;
+		public static final String GAMEFRAME_TITLE ="GAME MAKER";
+		
+		//SplitPane
+		public static int SPLITPANE_LOCATION= 200;
+		public static int SPLITPANE_SIZE = 5;
+				
+		//Ball sprite image size
+			public static final int BALL_RADIUS = 30;
+			
+			//Paddle sprite image size
+			public static final int PADDLE_WIDTH = 60;
+			public static final int PADDLE_HEIGHT = 20;
+			
+			//Brick sprite image size
+			public static final int BRICK_WIDTH = 60;
+			public static final int BRICK_HEIGHT = 40;
+			
+			//Strings for Actions
+			public static final String MOVE = "Move";
+			public static final String AUTOMOVE = "Automove";
+			public static final String BLOWUP = "Blow Up";
+			
+			
+			//Strings for Events
+			public static final String SIDES = "Side";
+			public static final String UPDOWN = "Up";
+			public static final String COLLISION = "Collision";
+			
+			//Strings for Game Buttons
+			public static final String DRAW = "Draw";
+			public static final String PLAY = "Play";
+			public static final String PAUSE = "Pause";
+			public static final String SAVE = "Save";
+			public static final String LOAD = "Load";
+			
+			/*//Default Draw position
+			public static int DEFAULT_X = 500;
+			public static int DEFAULT_Y = 500;*/
+			
+			//Default Move value
+			public static int DEFAULT_MOVE = 20;
+			
+			//Non Move Value
+			public static int NO_MOVE = 1000;
+			
+			//Strings for Images
+			public static final String AQUA = "Aqua";
+			public static final String FOOT = "Foot";
+			public static final String PADDLE = "Paddle";
+			public static final String BRICK = "Brick";
+			
+			
+			//Move speed for the ball
+			public static int BALL_SPEED_X = 5;
+			public static int BALL_SPEED_Y = 5;
+			
+			public static final String BLUE = "Blue";
+			public static final String GREEN = "Green";
+			public static final String YELLOW = "Yellow";
+			
+			/*//Game Bound dimension
+			public static final int BOUND_WIDTH = GAME_PANEL_WIDTH+100;
+			public static final int BOUND_HEIGHT = GAME_PANEL_HEIGHT-40;
+			*/
+			//background sound music 
+			public static final String MARIOMUSIC = "Mario";
+			public static final String TICKTOCK = "Tick Tock";
+			public static final String NOMUSIC = "No Music";
+	    
+		/*//speed of the auto sprite
+		public static int MOVEMENT_AUTO_X = 15;
+		public static int MOVEMENT_AUTO_Y = 15;*/
+		
+		//speed of command sprite
+		public static int MOVEMENT_CMD_X = 5;
+		public static int MOVEMENT_CMD_Y = 5;
+		
+
+	/*	
+	    //default size of panel
+		public static int PANEL_WIDTH = 700;
+		public static int PANEL_HEIGTH = 700;
+		
+		public static int BALL_HEIGHT = 40;
+		public static int BALL_WIDTH = 40;*/
+		
+
+	
+
 	
 	//Default location of Sprite
 	public static final int DEFAULT_X = 300;
@@ -30,9 +125,9 @@ public class Constants {
 	public static int MOVE_PLAYER_X = 5;
 	public static int MOVE_PLAYER_Y = 5;
 	
-	//size for paddle sprite
+/*	//size for paddle sprite
 	public static int PADDLE_HEIGHT = 50;
-	public static int PADDLE_WIDTH = 200;
+	public static int PADDLE_WIDTH = 200;*/
 	
 	//size of frame
 	public static int FRAME_WIDTH=1400;
@@ -45,7 +140,7 @@ public class Constants {
 	public static int BALL_HEIGHT = 40;
 	public static int BALL_WIDTH = 40;
 	
-	public static int BRICK_HEIGHT = 40;
-	public static int BRICK_WIDTH = 60;
+	/*public static int BRICK_HEIGHT = 40;
+	public static int BRICK_WIDTH = 60;*/
 	
 }
