2badbf706a845fc48c6c9bbe9fa34669ef755e11
Niteesh01 <niteesh.iit@gmail.com>
2017-10-08 00:54:16 -0400

    F2017A6T1-25:http://tintin.cs.indiana.edu:8091/browse/F2017A6T1-25
    "Added Background Music"

--- src/main/java/com/game/helpers/SoundButtonListener.java
+++ src/main/java/com/game/helpers/SoundButtonListener.java
@@ -2,16 +2,49 @@ package com.game.helpers;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.Serializable;
 
-public class SoundButtonListener implements ActionListener {
+import org.apache.log4j.Logger;
+
+import com.game.strategy.Music;
+
+import javazoom.jl.player.Player;
+
+public class SoundButtonListener implements ActionListener, Serializable {
+	private Player player;
+	String fileName;
+
+	private static Logger backgroundMusicRbLog = Logger.getLogger("backgroundMusicRbLogger");
+
+	Music music = new Music();
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
 		String commandString = e.getActionCommand();
-		switch(commandString){
-
+		switch (commandString) {
+		case Constants.MARIOMUSIC:
+			fileName = "sound/mario.mp3";
+			stopMusic();
+			playMusic(fileName);
+			break;
+		case Constants.TICKTOCK:
+			fileName = "sound/Tick.mp3";
+			stopMusic();
+			playMusic(fileName);
+			break;
+		case Constants.NOMUSIC:
+			stopMusic();
+			break;
 		}
 	}
 
+	private void playMusic(String fileName) {
+		music.play(fileName);
+	}
+
+	private void stopMusic() {
+		music.stop();
+	}
+
 }
+
